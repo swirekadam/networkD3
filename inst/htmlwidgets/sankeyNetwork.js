a87b3f8ecc5be6c8a21ba53073bc36021c43a2b4
swirekadam <1819941+swirekadam@users.noreply.github.com>
2018-05-21 21:03:25 +0200

    body -> div

--- inst/htmlwidgets/sankeyNetwork.js
+++ inst/htmlwidgets/sankeyNetwork.js
@@ -169,7 +169,7 @@ HTMLWidgets.widget({
         // note: u2192 is right-arrow
         link.append("title")
             .append("foreignObject")
-            .append("xhtml:body")
+            .append("div")
             .html(function(d) { return "<pre>" + d.source.name + " \u2192 " + d.target.name +
                 "\n" + format(d.value) + " " + options.units + "</pre>"; });
 
@@ -183,7 +183,7 @@ HTMLWidgets.widget({
             .style("cursor", "move")
             .append("title")
             .append("foreignObject")
-            .append("xhtml:body")
+            .append("div")
             .html(function(d) { return "<pre>" + d.name + "<br>" + format(d.value) + 
                 " " + options.units + "</pre>"; });
 
